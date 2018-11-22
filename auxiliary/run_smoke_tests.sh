da2ad9e2877544165581457385d003af66f66f70
Artyom Y. Razinov <arazinov@avito.ru>
2018-11-22 21:35:30 +0300

    Update run_smoke_tests.sh to install missing dependencies

--- auxiliary/run_smoke_tests.sh
+++ auxiliary/run_smoke_tests.sh
@@ -2,11 +2,64 @@
 
 set -e
 
-which pytest || pip3 install pytest
+main() {
+    goToWorkingDirectory
+    checkDependencies
+    runTests
+}
 
-cd `dirname $0`
-cd `git rev-parse --show-toplevel`
-cd "IntegrationTests"
+goToWorkingDirectory() {
+    cd `dirname $0`
+    cd `git rev-parse --show-toplevel`
+    cd "IntegrationTests"
+}
 
-pip3 install -r "requirements.txt"
-pytest --cache-clear
+checkDependencies() {
+    upgradePythonIfNeeded
+    installPyTestIfNeeded
+    installRequirementsTxt
+}
+
+runTests() {
+    pytest --cache-clear
+}
+
+upgradePythonIfNeeded() {
+    local pythonVersion=`python3 --version`
+
+    # Require Python >= 3.6
+    if [ `echo "$pythonVersion"|perl -pe 's/^.*?([0-9]+)\.([0-9]+).*$/\1\2/'` -ge 36 ]
+    then
+        echo "Python is already installed, python3 --version is $pythonVersion"
+    else
+        echo "Incorrect python version, upgrading python..."
+        upgradePython
+    fi
+}
+
+upgradePython() {
+    if brew upgrade python 2>&1 | grep "Install the Command Line Tools"
+    then
+        echo "Command Line Tools not installed, installing..."
+        installCommandLineTools
+        brew upgrade python
+    fi
+}
+
+installCommandLineTools() {
+    touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+
+    packageName=$(softwareupdate -l|grep "\*.*Command Line"|head -n 1|awk -F"*" '{print $2}'|sed -e 's/^ *//' | tr -d '\n')
+
+    softwareupdate -i "$packageName"
+}
+
+installPyTestIfNeeded() {
+    which pytest || pip3 install pytest
+}
+
+installRequirementsTxt() {
+    pip3 install -r "requirements.txt"
+}
+
+main
